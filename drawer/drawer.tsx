768b739602352e22a6ab585fc72ff152307e9a9b
Filip Krawczyk <kotmawszy@gmail.com>
2023-09-15 09:17:32 +0200

    Change peaks drawing

--- drawer/drawer.tsx
+++ drawer/drawer.tsx
@@ -49,25 +49,31 @@ export function Drawer({ peaks, ...props }: DrawerProps) {
     }, [props.hash?.hasher]);
 
     return (
-        <div className="max-w-full overflow-x-auto overflow-y-hidden">
-            <div className="-mb-3 w-max px-10 py-4">
-                <CustomDrawer
-                    {...props}
-                    _updateIndex={updateIndex}
-                    _isParentVirtual={true}
-                />
-                {peaks !== undefined && (
-                    <>
-                        <p className="w-60 py-4 text-center">Peaks</p>
-                        <PeakDrawer
-                            peaks={peaks}
-                            start={0}
-                            colorSettings={props.colorSettings}
-                        />
-                    </>
-                )}
+        <>
+            <div className="max-w-full overflow-x-auto overflow-y-hidden">
+                <div className="-mb-3 w-max px-10 py-4">
+                    <CustomDrawer
+                        {...props}
+                        _updateIndex={updateIndex}
+                        _isParentVirtual={true}
+                    />
+                </div>
             </div>
-        </div>
+            <div className="max-w-full overflow-x-auto overflow-y-hidden">
+                <div className="relative mb-6 w-max py-4 pl-28 pr-10">
+                    {peaks !== undefined && (
+                        <>
+                            <p className="w-12 py-4 text-center">Peaks</p>
+                            <PeakDrawer
+                                peaks={peaks}
+                                start={0}
+                                colorSettings={props.colorSettings}
+                            />
+                        </>
+                    )}
+                </div>
+            </div>
+        </>
     );
 }
 
@@ -165,44 +171,32 @@ function PeakDrawer({ peaks, start, colorSettings }: PeakDrawerProps) {
     if (start == peaks[0].length - 1)
         return (
             <div
-                className={`grid w-full grid-cols-[6rem_3rem_1fr] grid-rows-[auto_3rem_auto] ${
-                    start != 0 ? `translate-x-[-4.5rem]` : ``
-                }`}
+                className="relative z-10 flex aspect-square w-12 items-center justify-center rounded-full border-[3px] bg-black transition-colors"
+                style={{
+                    borderColor: colorSettings[start == 0 ? `root` : `peak`],
+                }}
             >
-                <div />
-                <div
-                    className="relative z-10 mx-auto flex aspect-square w-12 items-center justify-center rounded-full border-[3px] bg-black transition-colors"
-                    style={{
-                        borderColor:
-                            colorSettings[start == 0 ? `root` : `peak`],
-                    }}
-                >
-                    {start == 0 ? (
-                        <span className="text-xs">root</span>
-                    ) : (
-                        <span>{index}</span>
-                    )}
-                    <span className="z-12 group absolute top-12 rounded bg-neutral-800 p-1 text-sm">
-                        {hash.substring(0, 8)}
-                        {hash.length > 8 && `...`}
-                        <input
-                            value={hash}
-                            disabled
-                            className="absolute bottom-full right-1/2 z-50 hidden max-w-[8rem] translate-x-1/2 rounded border border-white bg-black p-2 text-center group-hover:block"
-                        />
-                    </span>
-                </div>
+                {start == 0 ? (
+                    <span className="text-xs">root</span>
+                ) : (
+                    <span>{index}</span>
+                )}
+                <span className="z-12 group absolute top-12 rounded bg-neutral-800 p-1 text-sm">
+                    {hash.substring(0, 8)}
+                    {hash.length > 8 && `...`}
+                    <input
+                        value={hash}
+                        disabled
+                        className="absolute bottom-full right-1/2 z-50 hidden max-w-[8rem] translate-x-1/2 rounded border border-white bg-black p-2 text-center group-hover:block"
+                    />
+                </span>
             </div>
         );
+
     return (
-        <div
-            className={`grid w-full grid-cols-[6rem_3rem_1fr] grid-rows-[auto_3rem_auto] ${
-                start != 0 ? `translate-x-[-4.5rem]` : ``
-            }`}
-        >
-            <div />
+        <div className="relative">
             <div
-                className="relative z-10 flex aspect-square items-center justify-center rounded-full border-[3px] bg-black transition-colors"
+                className="relative z-10 flex aspect-square w-12 items-center justify-center rounded-full border-[3px] bg-black transition-colors"
                 style={{
                     borderColor:
                         colorSettings[start == 0 ? `root` : `standard`],
@@ -219,27 +213,23 @@ function PeakDrawer({ peaks, start, colorSettings }: PeakDrawerProps) {
                     />
                 </span>
             </div>
-            <div />
 
-            <div className="relative w-full">
-                <div className="absolute -inset-y-6 -right-6 left-1/2">
-                    <Line side="left" color={colorSettings.standard} />
-                </div>
+            <div className="absolute -left-12 top-6 h-24 w-[4.5rem]">
+                <Line side="left" color={colorSettings.standard} />
             </div>
             <div />
-            <div className="relative w-24">
-                <div className="absolute -inset-y-6 -left-6 right-1/2">
-                    <Line side="right" color={colorSettings.standard} />
-                </div>
+            <div className="-mt-6 ml-6 h-24 w-[4.5rem]">
+                <Line side="right" color={colorSettings.standard} />
             </div>
+
             <div
-                className="relative z-10 mx-auto flex aspect-square w-12 items-center justify-center rounded-full border-[3px] bg-black transition-colors"
+                className="absolute left-[-4.5rem] top-24 z-10 flex aspect-square w-12 items-center justify-center rounded-full border-[3px] bg-black transition-colors"
                 style={{
                     borderColor: colorSettings.peak,
                 }}
             >
                 <span>{index}</span>
-                <span className="z-12 group absolute top-12 rounded bg-neutral-800 p-1 text-sm">
+                <span className="group absolute top-12 rounded bg-neutral-800 p-1 text-sm">
                     {hash.substring(0, 8)}
                     {hash.length > 8 && `...`}
                     <input
@@ -249,8 +239,10 @@ function PeakDrawer({ peaks, start, colorSettings }: PeakDrawerProps) {
                     />
                 </span>
             </div>
-            <div />
-            <PeakDrawer {...{ peaks, colorSettings }} start={start + 1} />
+
+            <div className="-mt-6 ml-[4.5rem]">
+                <PeakDrawer {...{ peaks, colorSettings }} start={start + 1} />
+            </div>
         </div>
     );
 }
